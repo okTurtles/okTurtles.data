12f5d256bdda2d6eadd45246d50c086d3305599a
Greg Slepak <contact@taoeffect.com>
2022-02-03 19:43:40 -0800

    reduced size of  by updating

--- dist/main.cjs
+++ dist/main.cjs
@@ -11,22 +11,22 @@ var _sbp = _interopRequireDefault(require("@sbp/sbp"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var _store = new Map();
+const _store = new Map();
 
 var _default = (0, _sbp.default)('sbp/selectors/register', {
-  'okTurtles.data/get': function okTurtlesDataGet(key) {
+  'okTurtles.data/get': function (key) {
     return _store.get(key);
   },
-  'okTurtles.data/set': function okTurtlesDataSet(key, data) {
+  'okTurtles.data/set': function (key, data) {
     _store.set(key, data);
 
     return data;
   },
-  'okTurtles.data/delete': function okTurtlesDataDelete(key) {
+  'okTurtles.data/delete': function (key) {
     return _store.delete(key);
   },
-  'okTurtles.data/add': function okTurtlesDataAdd(key, data) {
-    var array = _store.get(key);
+  'okTurtles.data/add': function (key, data) {
+    const array = _store.get(key);
 
     if (array) {
       array.push(data);
@@ -34,21 +34,19 @@ var _default = (0, _sbp.default)('sbp/selectors/register', {
       _store.set(key, [data]);
     }
   },
-  'okTurtles.data/remove': function okTurtlesDataRemove(key, data) {
-    var array = _store.get(key);
+  'okTurtles.data/remove': function (key, data) {
+    const array = _store.get(key);
 
     if (array) {
-      var aLen = array.length;
-      var filtered = array.filter(function (v) {
-        return v !== data;
-      });
+      const aLen = array.length;
+      const filtered = array.filter(v => v !== data);
 
       _store.set(key, filtered);
 
       return aLen - filtered.length;
     }
   },
-  'okTurtles.data/apply': function okTurtlesDataApply(key, fn) {
+  'okTurtles.data/apply': function (key, fn) {
     return fn(_store.get(key));
   }
 });
